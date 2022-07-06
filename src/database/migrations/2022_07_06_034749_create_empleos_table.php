56f33902cc935c8937b48674213772fd1fd29b00
bruchane91 <bruchane91@gmail.com>
2022-07-05 22:49:26 -0500

    Migraciones

---
+++ src/database/migrations/2022_07_06_034749_create_empleos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleos', function (Blueprint $table) {
+            $table->id();
+            $table->string('rol');
+            $table->integer('experiencia');
+            $table->integer('sueldo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleos');
+    }
+}
